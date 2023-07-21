efb8ec0e2e5891cd5f8b3d22187c3d07389b1ded
Ashton Engberg <atengberg@proton.me>
2023-07-21 11:55:07 -0400

    fixed firefox check

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -18,13 +18,13 @@ const dapp = (
 
 const notSupported = (
   <div className="mt-[21%] w-full text-center text-3xl italic">
-    This dapp requires a browser that supports web workers (try Brave).
+    This dapp requires a browser that supports ESM module type web workers (try Brave).
   </div>
 );
 
 // Check web workers supported; Firefox doesn't yet by default allow module type workers.
 // See https://github.com/vitejs/vite/issues/12874 for more info.
-const supported = !!typeof(Worker) || !(navigator.userAgent.toLowerCase().indexOf('firefox') > -1)
+const supported = !(navigator.userAgent.toLowerCase().indexOf('firefox') > -1)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   supported ? dapp : notSupported 
