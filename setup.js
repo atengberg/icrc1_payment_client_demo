bd62e5d3f64d6c3220b583d9d3144829cdad451b
Ashton Engberg <atengberg@proton.me>
2023-07-26 13:07:42 -0400

    simplified setup script

--- setup.js
+++ setup.js
@@ -9,7 +9,6 @@ import {
   getCanisterId,
   getICRC1TokenCanisterDfxDeploymentCmd,
   getICRC1TransferDfxCmd,
-  appendModeIsTestingEnvVar
 } from './script-utils.js';
 
 // Load the environmental variables from the .env file:
@@ -81,12 +80,6 @@ async function run(testing = false) {
   } catch (e) {
     throw new Error(`While transferring to current user's subaccount, caught error ${e.toString()}`);
   };
-
-  try {
-    appendModeIsTestingEnvVar(testing);
-  } catch (e) {
-    throw new Error(`Could not append MODE_IS_TESTING env var due to error ${e}`);
-  }
 
   if (testing) {
     // nns ed25519:
--- src/test/vitest.config.js
+++ src/test/vitest.config.js
@@ -10,7 +10,8 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 // config's environmental variables loading, so import the .env
 // and parse it to spread on define (below) so it'll be available.
 // This is another way of making DFX_NETWORK, and other env vars
-// available. 
+// available. Note, could also be defined on process.env, OR 
+// dotenv could be used in the setup-teardown-hooks. 
 const importAsMetaEnvVars = fs.readFileSync(path
   .join(__dirname, '..', '..', '.env'), { encoding: 'utf8' })
   .split('\n').reduce((acc, cur) => {
@@ -19,7 +20,6 @@ const importAsMetaEnvVars = fs.readFileSync(path
     return {
       ...acc,
       [`import.meta.env.${kv[0]}`]: JSON.stringify(kv[1]),
-      [`process.env.${kv[0]}`]: JSON.stringify(kv[1])
     };
   } else {
     return acc;
@@ -43,16 +43,20 @@ export default defineConfig({
     ...importAsMetaEnvVars,
     'import.meta.env.WORKER_PATH': JSON.stringify(workerPath),
     'import.meta.env.WORKER_HELLO_WORLD': JSON.stringify('Hello from the Web Worker unit test!'),
-    'import.meta.env.OG_WORKER_PATH': JSON.stringify(originalWorkerPath)
+    'import.meta.env.OG_WORKER_PATH': JSON.stringify(originalWorkerPath),
+    'import.meta.env.MODE_IS_TESTING': JSON.stringify(true),
+    'import.meta.env.HOST': JSON.stringify('127.0.0.1:4943'),
+    // INDEXEDDB Won't work in tests:
+    'import.meta.env.DISABLE_INDEXEDB': JSON.stringify(true),
   },
   test: {
-    globals:true, 
+    threads: false,
     setupFiles: ['./setup-teardown-hooks.js'],
     environment: 'jsdom',
     testTimeout: 15000,
     include: [
-      'src/e2e/*.test.jsx',
-      //'src/unit/frontend/*.test.js',
+      'src/e2e/*.test.js',
+      'src/unit/frontend/*.test.js',
     ],
   }
 });
